f948cce07d8885d279cafbb79344e41d40a608d8
bed-deb <julus06@o2.pl>
2025-08-14 18:46:54 +0200

    Update login2.js

--- login2.js
+++ login2.js
@@ -2,7 +2,7 @@ function login() {
         var password = document.getElementById('password').value;
 
         // Tablica przechowująca poprawne hasła
-        var correctPasswords = ["Mt_8!j$Xr@w#Lp"]; // Możesz dodać więcej haseł tutaj
+        var correctPasswords = ["Pt_6!h$Kq@d#Lm"]; // Możesz dodać więcej haseł tutaj
 
         // Sprawdzenie, czy wprowadzone hasło znajduje się w tablicy
         if (password === "") {
